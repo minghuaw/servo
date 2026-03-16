3b5f9f8a4a02e1433176a21376f56d2dd1392d9f
Minghua Wu <michael.wu1107@gmail.com>
2026-03-16 12:39:08 +0800

    check loaded font
    Signed-off-by: Minghua Wu <michael.wu1107@gmail.com>

--- components/fonts/font_context.rs
+++ components/fonts/font_context.rs
@@ -769,6 +769,17 @@ impl FontContext {
             .iter()
             .rev()
             .filter(Self::is_supported_web_font_source)
+            .filter(|source| {
+                if let Source::Url(url) = source {
+                    url.url.url()
+                        .cloned()
+                        .map(ServoUrl::from)
+                        .map(FontIdentifier::Web)
+                        .is_some_and(|url| !self.font_data.read().contains_key(&url))
+                } else {
+                    true
+                }
+            })
             .cloned()
             .collect();
 
